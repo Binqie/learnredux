7f761fcf8f6bd02f504073a48288afc5cd343f8d
Binqie <shamir.beishebaev@icloud.com>
2022-12-03 18:14:34 +0600

    reducers were separeted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import './App.css';
-import {useDispatch, useSelector} from 'react-redux'
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addCustomerAction, removeCustomerAction } from "./store/customerReducer";
+import { addCashAction, getCashAction } from "./store/cashReducer";
 
 function App() {
-  const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash)
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash.cash);
+  const customers = useSelector((state) => state.customers.customers);
 
   const addCash = (cash) => {
-    dispatch({type: "ADD_CASH", payload: cash})
-  }
+    dispatch(addCashAction(cash));
+  };
 
   const getCash = (cash) => {
-    dispatch({type: "GET_CASH", payload: cash})
-  }
+    dispatch(getCashAction(cash));
+  };
+
+  const addCustomer = (name) => {
+    const customer = {
+      name: name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customer));
+  };
+
+  const removeCustomer = (id) => {
+    dispatch(removeCustomerAction(id));
+  };
 
   return (
-    <div className="App" style={{'display': 'flex', 'flexDirection': 'column', 'height': '100vh', 'justifyContent': 'center', 'alignItems': 'center'}}>
-      <div className='cash'>{cash}</div>
-      <div className='btns'>
-        <button onClick={() => addCash(Number(prompt()))}>add</button>
-        <button onClick={() => getCash(Number(prompt()))}>get</button>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="cash">{cash}</div>
+      <div className="btns">
+        <button onClick={() => addCash(Number(prompt()))}>add cash</button>
+        <button onClick={() => getCash(Number(prompt()))}>get cash</button>
+      </div>
+      <div>
+        {customers.length > 0 ? (
+          <div>
+            {customers.map((customer) => (
+              <div
+                style={{'cursor': 'pointer', 'color': 'white'}}
+                onClick={() => removeCustomer(customer.id)}>{customer.name}</div>
+            ))}
+          </div>
+        ) : (
+          <div>No customers yet</div>
+        )}
+      </div>
+      <div className="btns">
+        <button onClick={() => addCustomer(prompt())}>add customer</button>
       </div>
     </div>
   );
